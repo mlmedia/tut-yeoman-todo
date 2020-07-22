639ba02cd4bb81851d9077e0cccb0b051c0cff62
mlmedia <mike@millionlightsmedia.com>
2020-07-21 23:42:21 -0400

    - change to about.js (unknown)

--- mytodo/app/scripts/controllers/about.js
+++ mytodo/app/scripts/controllers/about.js
@@ -8,7 +8,7 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-	.controller('AboutCtrl', function($scope) {
+	.controller('AboutCtrl', function ($scope) {
 		$scope.awesomeThings = [
 			'HTML5 Boilerplate',
 			'AngularJS',
